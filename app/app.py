1a5ddbec501035e4feabad54b7d7a750b3199b2d
KobeMadeTech <139927225+KobeCyber@users.noreply.github.com>
2025-09-18 14:57:43 -0500

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name)
+
+@app.route("/")
+def home():
+  return " Flask app has been deployed"
+if __name__ == "__main__":
+  app.run(host="0.0.0.0, port=5000)
