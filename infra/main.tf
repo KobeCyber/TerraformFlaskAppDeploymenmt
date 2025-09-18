3047139f4e5e6995c650c3ccb325fbbd2a87ec8f
KobeMadeTech <139927225+KobeCyber@users.noreply.github.com>
2025-09-18 16:31:04 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "flask_ec2" {
               usermod -aG docker ec2-user
               cd /home/ec2-user
               yum install git -y
-              git clone https://github.com/YOUR_USERNAME/YOUR_REPO.git flask-app
+              git clone https://github.com/KobeCyber/TerraformFlaskAppDeploymenmt.git flask-app
               cd /app
               docker build -t flask-app .
               docker run -d -p 80:5000 flask-app
