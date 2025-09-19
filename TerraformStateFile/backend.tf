cdcfe3ff9f1f9f18e2ea935b4a0fbb79bf0635ba
KobeMadeTech <139927225+KobeCyber@users.noreply.github.com>
2025-09-19 15:04:10 -0500

    Update backend.tf

--- TerraformStateFile/backend.tf
+++ TerraformStateFile/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "project-terraform-state-bucket"
+    bucket         = "kobecyber-terraform-state-bucket"
     key            = "filepath/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "project-terraform-lock-table"
+    dynamodb_table = "kobecyber-terraform-lock-table"
     encrypt        = true
   }
 }
