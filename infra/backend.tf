dd07786fd7279018b2a98b86304e03a86b66a524
KobeMadeTech <139927225+KobeCyber@users.noreply.github.com>
2025-09-19 15:22:13 -0500

    Delete infra/backend.tf

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "project-terraform-state-bucket"
-    key            = "filepath/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "project-terraform-lock-table"
-    encrypt        = true
-  }
-}
