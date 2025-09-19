c37996f78b7791e9e2763645200b02816fa96389
KobeMadeTech <139927225+KobeCyber@users.noreply.github.com>
2025-09-19 15:57:12 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_content
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return " Flask app has been deployed"
+    return render_content("web.html")
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
